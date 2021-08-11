9140a4dff78b1a40c798bad7980726d5710e850d
nandinichhajed <nandinichhajed08@gmail.com>
2021-08-11 23:40:50 +0530

    Creating Django Forms

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post      # The dot before models means current directory or current application
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,3 +11,8 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
