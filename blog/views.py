b8fd0f0aba50cfc9ae23dec0d278abfcb2221cd3
nandinichhajed <nandinichhajed08@gmail.com>
2021-08-12 17:28:37 +0530

    Code to add new post in the form

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post      # The dot before models means current directory or current application
 from django.shortcuts import render, get_object_or_404
@@ -13,6 +13,15 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
